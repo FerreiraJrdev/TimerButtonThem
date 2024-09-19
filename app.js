46f2a1b3ccd71f33b85cc7cbff25b036af338a2f
Junior <141884886+FerreiraJrdev@users.noreply.github.com>
2024-09-19 11:47:58 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+const switcher = document.querySelector('.btn');
+switcher.addEventListener('click', function () {
+  document.body.classList.toggle('dark-theme')
+
+  var className = document.body.className;
+  if (className == "light-theme") {
+    this.textContent = "Dark";
+  }
+  else {
+    this.textContent = "Light";
+  }
+});
+
+function relogio() {
+  function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+      hour12: false,
+      timeZone: 'UTC'
+    });
+  }
+
+  const relogio = document.querySelector('#relogio');
+  let segundos = 0;
+  let timer;
+
+  function iniciaRelogio() {
+    timer = setInterval(function () {
+      segundos++;
+      relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000);
+  }
+
+  document.addEventListener('click', function (e) {
+    const el = e.target;
+
+    if (el.classList.contains('zerar')) {
+      clearInterval(timer);
+      relogio.innerHTML = '00:00:00';
+      relogio.classList.remove('pausado');
+      segundos = 0;
+    }
+
+    if (el.classList.contains('iniciar')) {
+      relogio.classList.remove('pausado');
+      clearInterval(timer);
+      iniciaRelogio();
+    }
+
+    if (el.classList.contains('pausar')) {
+      clearInterval(timer);
+      relogio.classList.add('pausado');
+    }
+  });
+}
+
+relogio();
